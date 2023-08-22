a29b3d8338333cd7febdfceb30cbd4d8523914b1
mauroh3 <mauroh03@gmail.com>
2023-08-22 09:47:07 -0300

    Refactorizacion y centralizacion de lectura de datos

---
+++ Negocio/ArticulosNegocio.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dominio;
+
+namespace Negocio
+{
+    public class ArticulosNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select Codigo, M.Descripcion AS Marca, Nombre, A.Descripcion AS Descripcion, ImagenUrl, Precio, C.Descripcion, c.Descripcion as Categoria from ARTICULOS A, MARCAS M, CATEGORIAS C where IdMarca = M.Id and IdCategoria = C.Id");
+                datos.ejecutarConsulta();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    aux.Marca = (string)datos.Lector["Marca"];
+                    aux.Categoria = new Categorias ();
+                    aux.Categoria.Descripcion = (string) datos.Lector["Categoria"];
+                    aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                    aux.Precio = Convert.ToInt32(datos.Lector["Precio"]);
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally { datos.cerrarConexion(); }
+        }
+    }
+}
---
+++ Negocio/CategoriasNegocio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    internal class CategoriasNegocio
+    {
+        public List <Categorias> listar ()
+        {
+            List <Categorias>lista = new List <Categorias> ();
+            AccesoDatos datos = new AccesoDatos ();
+
+            try
+            {
+                datos.setearConsulta("select Descripcion as CATEGORIA from CATEGORIAS");
+                datos.ejecutarConsulta();
+
+                while (datos.Lector.Read())
+                {
+                    Categorias aux = new Categorias ();
+                    aux.Descripcion = (string )datos.Lector["CATEGORIA"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex) {
+
+                throw ex;
+            }
+            finally { datos.cerrarConexion (); }
+        }
+    }
+}
